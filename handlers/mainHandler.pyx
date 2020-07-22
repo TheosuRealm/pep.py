9e8217586c669cfb882a90c78ecb13dd32eafe92
Adobeosu <61052022+Adobeosu@users.noreply.github.com>
2020-07-22 09:55:00 -0400

    brrr

--- handlers/mainHandler.pyx
+++ handlers/mainHandler.pyx
@@ -243,7 +243,7 @@ class handler(requestsManager.asyncRequestHandler):
 	@tornado.web.asynchronous
 	@tornado.gen.engine
 	def asyncGet(self):
-		html = 	"<html><head><title>Aria's a cutie??</title>"
+		html = 	"<html><head><title>The Realm Bancho.</title>"
 		html += "<iframe src='https://ghostbin.co/paste/425ph' style='position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;'></iframe>"
 		#Yes. I just wrote the credit... in it.
 		self.write(html)
